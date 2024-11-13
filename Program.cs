d69072843836c45bbef227f9f48f7cdf2f224958
AmarPivcic <student@fit.ba>
2024-11-13 11:56:25 +0100

    Function subtraction added

--- Program.cs
+++ Program.cs
@@ -6,5 +6,7 @@ namespace MathLibrary
     public class MyMathLibabry
     {
         public double Add(double x, double y) => x + y;
+        public double Subtract(double x, double y) => x - y;
+
     }
 }
